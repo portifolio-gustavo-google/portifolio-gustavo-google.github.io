47fc3c883d754f81738675e4553b1a96bbfdf9fe
Gustaocta <gustavo.otacilio@grupotecnoset.com.br>
2023-09-20 11:43:43 -0300

    first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,15 @@
+const menu = document.querySelector("#btn-menu")
+const luck = document.querySelector("form [type=\"button\"]");
+
+menu.addEventListener("click", () => {
+
+    console.log("Clicou");
+
+});
+
+luck.addEventListener("click", () => {
+
+    // console.log("Você está com sorte!");
+    location.href = "https://class.hcode.com.br"
+
+});
